64a4c79a6ef07322b460f3728ad742c66a3dbae9
coolwindblows <coolwindblows@protonmail.com>
2020-12-02 16:05:20 +0500

    victory added

--- src/components/Body/Body-element/Body-medium-graph/Body-medium-graph.jsx
+++ src/components/Body/Body-element/Body-medium-graph/Body-medium-graph.jsx
@@ -4,8 +4,16 @@ import '../../../../App.scss';
 import iconFollower from './svg/icon-follower.svg';
 import iconTotal from './svg/icon-total.svg';
 import smallArrowDown from './svg/small-arrow-down.svg';
+import * as V from 'victory';
+import { VictoryBar } from 'victory';
 
 const BodyMediumGraph = () => {
+    const data = [
+        {quarter: 1, earnings: 13000},
+        {quarter: 2, earnings: 16500},
+        {quarter: 3, earnings: 14250},
+        {quarter: 4, earnings: 19000},
+    ];
 
     return (
         <div className="lineal-graph">
@@ -15,7 +23,16 @@ const BodyMediumGraph = () => {
                     <div className="box-date">
                         <span className="date"> 31/01/2020 – 06/02/2020 </span>
                     <img src={smallArrowDown} className="" alt=""/>
-                    </div> 
+                    <div style={{ paddingTop: '130px'}}>
+                    <VictoryBar
+                        data={data}
+                        // data accessor for x values
+                        x="quarter"
+                        // data accessor for y values
+                        y="earnings"
+                    />
+                    </div>
+                    </div>
                     <div className="box-legend">
                         <img src={iconTotal} className="" alt=""/>
                         <span className="small-text"> Total </span>
@@ -30,4 +47,3 @@ const BodyMediumGraph = () => {
 };
 
 export default BodyMediumGraph;
-
