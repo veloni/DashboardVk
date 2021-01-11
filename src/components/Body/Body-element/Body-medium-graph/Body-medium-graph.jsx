aed2ef50fedec5bf317de6cdd8b6e200b8cf96ed
egorko <dewvify@gmail.com>
2021-01-11 23:24:58 +0500

    adaptive

--- src/components/Body/Body-element/Body-medium-graph/Body-medium-graph.jsx
+++ src/components/Body/Body-element/Body-medium-graph/Body-medium-graph.jsx
@@ -2,7 +2,6 @@ import './Body-medium-graph.scss';
 import '../../../../vars_and_mixins/colors.scss';
 import iconFollower from './svg/icon-follower.svg';
 import iconTotal from './svg/icon-total.svg';
-import smallArrowDown from './svg/small-arrow-down.svg';
 import { VictoryLine, VictoryChart, VictoryScatter, VictoryAxis } from 'victory';
 
 const BodyMediumGraph = () => {
@@ -54,7 +53,6 @@ const BodyMediumGraph = () => {
                     <span className="main-text"> Audiende reach</span>
                     <div className="box-date">
                         <span className="date"> 31/01/2020 – 06/02/2020 </span>
-                    <img src={smallArrowDown} className="icon-date-switch" alt=""/>
                     </div>
                    
                     <div className="box-legend">
